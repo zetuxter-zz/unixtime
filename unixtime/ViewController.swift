ece3f7fc59ce322fc34d23e0bdf6653bef19ee31
Paul <paul@Pauls-Mac-mini.local>
2015-01-15 01:14:02 +0000

    various developments

--- unixtime/ViewController.swift
+++ unixtime/ViewController.swift
@@ -8,15 +8,21 @@
 
 import UIKit
 import iAd
+import Foundation
 
 class ViewController: UIViewController, ADBannerViewDelegate {
     
     @IBOutlet weak var adBannerView: ADBannerView!
     
+    var dateEdited=false
+    var tsEdited=false
+    
     var time:timeval=timeval(tv_sec: 0, tv_usec: 0)
-    var dohex:Bool=true
+    var dohex:Bool=false
     var timer = NSTimer()
 
+    @IBOutlet weak var errorText: UILabel!
+    
     @IBOutlet weak var autoManualValue: UISegmentedControl!
     
     @IBOutlet weak var hexDecValue: UISegmentedControl!
@@ -45,22 +51,138 @@ class ViewController: UIViewController, ADBannerViewDelegate {
         println("it changed")
     }
     
-  
+    @IBOutlet weak var recalcButton: UIButton!
+    
+    @IBAction func recalcButton(sender: AnyObject) {
+        
+        if (dateEdited) {
+            
+            var ds : String = yearTextField.text+"-"+monthTextField.text+"-"+dayTextField.text+" "+hourTextField.text+":"+minuteTextField.text+":"+secondTextField.text
+            NSLog(ds)
+            var dateFormatter = NSDateFormatter()
+            dateFormatter.dateFormat = "yyyy-M-d H:m:s"
+            
+            // convert string into date
+            
+            var nw:NSDate? = dateFormatter.dateFromString(ds)
+            // hack - fix populateNow
+            if (nw != nil) {
+                var nowf:NSDate=nw!
+                NSLog("%f", nowf.timeIntervalSince1970)
+                var now:Int=Int(nowf.timeIntervalSince1970)
+                
+                if dohex {
+                    tsTextField.text=String(format:"%x", now)
+                }
+                else {
+                    tsTextField.text=String(format:"%u", now)
+                }
+                errorText.text=""
+                self.view.endEditing(true)
+            }
+            else {
+                // it's invalid
+                errorText.text="Invalid date entered"
+            }
+        }
+        else {
+            // TS must have been edited most recently
+            // validate
+            if (!tsTextField.text.isEmpty) {
+                var now:NSDate
+                if (!dohex) {
+                    if tsTextField.text.toInt() != nil {
+                        var tsvi=tsTextField.text.toInt()!
+                        if (tsvi < 0 || tsvi > Int(INT_MAX)) {
+                            errorText.text="Valid TS values in Decimal mode are 0 to "+String(INT_MAX)
+                        }
+                        else {
+                            var tsv:NSTimeInterval=NSTimeInterval(tsvi)
+                            now=NSDate(timeIntervalSince1970: tsv)
+                            populateDisplay(Int(tsv), now:now)
+                            self.view.endEditing(true)
+                        }
+                    }
+                    else {
+                        errorText.text="TS out of 0 to "+String(INT_MAX)+" range"
+                    }
+                }
+                else {
+                    // hex validation
+                    var scanner=NSScanner(string: tsTextField.text)
+                    var result : UInt32 = 0
+                    if scanner.scanHexInt(&result) {
+                        if (result > 0x7fffffff) {
+                            errorText.text="TS out of 0 to 0x7fffffff range"
+                        }
+                        else {
+                            var tsv:NSTimeInterval=NSTimeInterval(result)
+                            now=NSDate(timeIntervalSince1970: tsv)
+                            populateDisplay(Int(tsv), now:now)
+                            self.view.endEditing(true)
+                        }
+                    }
+                    else {
+                        errorText.text="TS out of 0 to 0x7fffffff range"
+                    }
+                }
+            }
+        }
+    }
+    
+    func setDateEdited () {
+        dateEdited=true
+        tsEdited=false
+        recalcButton.setTitle("Date->TS", forState: UIControlState.Normal)
+    }
+
+    @IBAction func tsValueChanged(sender: AnyObject) {
+        NSLog("ts value change")
+        tsEdited=true
+        dateEdited=false
+        recalcButton.setTitle("TS->Date", forState: UIControlState.Normal)
+    }
+    
+    @IBAction func yearChanged(sender: AnyObject) {
+        NSLog("year changed")
+        setDateEdited()
+    }
+    
+    @IBAction func monthChanged(sender: AnyObject) {
+        setDateEdited()
+    }
+    
+    @IBAction func dayChanged(sender: AnyObject) {
+        setDateEdited()
+    }
+    
+    @IBAction func hourChanged(sender: AnyObject) {
+        setDateEdited()
+    }
+    
+    @IBAction func minuteChanged(sender: AnyObject) {
+        setDateEdited()
+    }
+    
+    @IBAction func secondChanged(sender: AnyObject) {
+        setDateEdited()
+    }
     
     func populateNow () {
         
         var now=NSDate()
-        var ti=now.timeIntervalSince1970;
-        
+        var tif=now.timeIntervalSince1970;
+        NSLog("populateNow %f", tif)
+        var ti:Int=Int(tif)
+        populateDisplay(ti, now:now)
+    }
+    
+    func populateDisplay(var ti:Int, var now:NSDate) {
         if dohex {
-            tsTextField.text=String(format:"%x", time.tv_sec)
-            //usecTextField.text=String(format:"%x", time.tv_usec)
+            tsTextField.text=String(format:"%x", ti)
         }
         else {
-            tsTextField.text=String(format:"%d", time.tv_sec)
-            //usecTextField.text=String(format:"%d", time.tv_usec)
-            //var zz:Int = (Int)(ti)
-            //usecTextField.text=String(format: "%d", zz)
+            tsTextField.text=String(format:"%u", ti)
         }
         let cal=NSCalendar.currentCalendar()
         let comp=cal.components((.YearCalendarUnit | .MonthCalendarUnit | .DayCalendarUnit | .HourCalendarUnit | .MinuteCalendarUnit | .SecondCalendarUnit), fromDate:now)
@@ -71,34 +193,48 @@ class ViewController: UIViewController, ADBannerViewDelegate {
         hourTextField.text=String(format: "%02d",comp.hour)
         minuteTextField.text=String(format: "%02d",comp.minute)
         secondTextField.text=String(format: "%02d", comp.second)
+        errorText.text=""
         
     }
     
     @IBAction func nowButton(sender: AnyObject) {
-        gettimeofday(&time, nil)
+        //gettimeofday(&time, nil)
         populateNow()
+        self.view.endEditing(true)
     }
     
     @IBOutlet weak var nowButton: UIButton!
     
     @IBAction func autoManualSelect(sender: AnyObject) {
+        var enabled:Bool
+        
         if autoManualValue.selectedSegmentIndex == 0 {
             // manual update
             timer.invalidate()
             nowButton.enabled=true
+            recalcButton.enabled=true
+            enabled=true
         }
         else {
             // auto update
-            timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: Selector("updTime"), userInfo: nil, repeats: true)
+            timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: Selector("updTime"), userInfo: nil, repeats: true)
             nowButton.enabled=false
+            recalcButton.enabled=false
+            enabled=false
         }
         populateNow()
-        
+        yearTextField.enabled=enabled
+        monthTextField.enabled=enabled
+        dayTextField.enabled=enabled
+        hourTextField.enabled=enabled
+        minuteTextField.enabled=enabled
+        secondTextField.enabled=enabled
+        tsTextField.enabled=enabled
     }
     
     func updTime()  {
         
-        gettimeofday(&time, nil)
+        //gettimeofday(&time, nil)
         
         populateNow()
         
@@ -106,12 +242,15 @@ class ViewController: UIViewController, ADBannerViewDelegate {
     
     @IBAction func hexDecSelect(sender: AnyObject) {
         if hexDecValue.selectedSegmentIndex == 0 {
-            dohex=true
+            dohex=false
+            tsTextField.keyboardType=UIKeyboardType.NumberPad
         }
         else {
-            dohex=false
+            dohex=true
+            tsTextField.keyboardType=UIKeyboardType.Default
         }
         populateNow()
+        self.view.endEditing(true)
     }
     
     
